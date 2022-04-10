138388fcc3390b38d53daea0bb50a708bf284684
yaroslavLarionov <yarillo@yahoo.com>
2022-04-09 23:32:29 -0400

    Here we are sorting the array in ascending order

---
+++ SortingArrayAscendingOrder.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class SortingArrayAscendingOrder {
+	public static void main(String[] args) {
+		
+		int[] arr = {8, 4, 6, 3, 2, 7};
+		int temp = 0;
+		System.out.println("Original array: " + Arrays.toString(arr));
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] > arr[j]) {
+					temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		System.out.println("Sorted array: " + Arrays.toString(arr));
+	}
+
+}
